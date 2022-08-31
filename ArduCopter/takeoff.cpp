86b9f4e452d6862095de4feedc6fe749c1c4a30b
Kunal Shrivastava <kunalshrivastava.iitr@gmail.com>
2022-08-31 18:20:55 +0530

    make alt takeoff exactly the same as althold

--- ArduCopter/takeoff.cpp
+++ ArduCopter/takeoff.cpp
@@ -239,43 +239,8 @@ void Mode::auto_takeoff_run()
 
 void Mode::alt_takeoff_run()
 {
-    // if not armed set throttle to zero and exit immediately
-    if (!motors->armed() || !copter.ap.auto_armed) {
-        // do not spool down tradheli when on the ground with motor interlock enabled
-        make_safe_ground_handling(copter.is_tradheli() && motors->get_interlock());
-//        wp_nav->shift_wp_origin_and_destination_to_current_pos_xy();
-        return;
-    }
-
-    copter.set_auto_armed(true);
-    // set motors to full range
-    motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::THROTTLE_UNLIMITED);
-
-    // process pilot's yaw input
-    float target_yaw_rate = 0;
-    if (!copter.failsafe.radio && copter.flightmode->use_pilot_yaw()) {
-        // get pilot's desired yaw rate
-        target_yaw_rate = get_pilot_desired_yaw_rate(channel_yaw->get_control_in());
-        if (!is_zero(target_yaw_rate)) {
-            auto_yaw.set_mode(AUTO_YAW_HOLD);
-        }
-    }
-
     if (!pos_control->is_active_z()) {
         pos_control->init_z_controller();
-    }
-
-    if (motors->get_spool_state() == AP_Motors::SpoolState::THROTTLE_UNLIMITED) {
-        set_land_complete(false);
-    } else {
-        // motors have not completed spool up yet so relax navigation and position controllers
-//        wp_nav->shift_wp_origin_and_destination_to_current_pos_xy();
-        pos_control->relax_z_controller(0.0f);   // forces throttle output to decay to zero
-        pos_control->update_z_controller();
-        attitude_control->reset_yaw_target_and_rate();
-        attitude_control->reset_rate_controller_I_terms();
-        attitude_control->input_euler_angle_roll_pitch_euler_rate_yaw(0.0f, 0.0f, 0.0f);
-        return;
     }
 
     // set vertical speed and acceleration limits
@@ -284,31 +249,60 @@ void Mode::alt_takeoff_run()
     // apply SIMPLE mode transform to pilot inputs
     update_simple_mode();
 
-    // get avoidance adjusted climb rate
-    float target_climb_rate = copter.mav_climb_rate;
-    // command the aircraft to the take off altitude and current pilot climb rate
-    Location target_loc = copter.current_loc;
-    Location::AltFrame frame = Location::AltFrame::ABOVE_HOME;
-    int32_t takeoff_alt_cms = 0;
-    float takeoff_targ = 0.0;
-    if(target_loc.get_alt_cm(frame, takeoff_alt_cms))
-    {
-      takeoff_targ = takeoff_alt_cms/100.0;
-    }
-    copter.pos_control->input_pos_vel_accel_z(takeoff_targ, target_climb_rate, 0);
-    target_climb_rate = constrain_float(target_climb_rate, -get_pilot_speed_dn(), g.pilot_speed_up);
-    target_climb_rate = get_avoidance_adjusted_climbrate(target_climb_rate);
-
-    // update the vertical offset based on the surface measurement
-    copter.surface_tracking.update_surface_offset();
-
-    // Send the commanded climb rate to the position controller
-    pos_control->set_pos_target_z_from_climb_rate_cm(target_climb_rate);
-
-    // call attitude controller
     // get pilot desired lean angles
     float target_roll, target_pitch;
     get_pilot_desired_lean_angles(target_roll, target_pitch, copter.aparm.angle_max, attitude_control->get_althold_lean_angle_max());
+
+    float target_yaw_rate = 0;
+
+    // get avoidance adjusted climb rate
+    float target_climb_rate = copter.mav_climb_rate;
+    // Alt Hold State Machine Determination
+    AltHoldModeState althold_state = get_alt_hold_state(target_climb_rate);
+    // Alt Hold State Machine
+    switch (althold_state) {
+
+    case AltHold_MotorStopped:
+        attitude_control->reset_rate_controller_I_terms();
+        attitude_control->reset_yaw_target_and_rate(false);
+        pos_control->relax_z_controller(0.0f);   // forces throttle output to decay to zero
+        break;
+
+    case AltHold_Landed_Ground_Idle:
+        attitude_control->reset_yaw_target_and_rate();
+        FALLTHROUGH;
+
+    case AltHold_Landed_Pre_Takeoff:
+        attitude_control->reset_rate_controller_I_terms_smoothly();
+        pos_control->relax_z_controller(0.0f);   // forces throttle output to decay to zero
+        break;
+
+    case AltHold_Takeoff:
+        // initiate take-off
+        if (!takeoff.running()) {
+            takeoff.start(constrain_float(g.pilot_takeoff_alt,0.0f,1000.0f));
+        }
+
+        // get avoidance adjusted climb rate
+        target_climb_rate = get_avoidance_adjusted_climbrate(target_climb_rate);
+
+        // set position controller targets adjusted for pilot input
+//        takeoff.do_pilot_takeoff(target_climb_rate);
+        FALLTHROUGH;
+
+    case AltHold_Flying:
+        motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::THROTTLE_UNLIMITED);
+
+        // get avoidance adjusted climb rate
+        target_climb_rate = get_avoidance_adjusted_climbrate(target_climb_rate);
+
+        // update the vertical offset based on the surface measurement
+        copter.surface_tracking.update_surface_offset();
+
+        // Send the commanded climb rate to the position controller
+        pos_control->set_pos_target_z_from_climb_rate_cm(target_climb_rate);
+        break;
+    }
 
     attitude_control->input_euler_angle_roll_pitch_euler_rate_yaw(target_roll, target_pitch, target_yaw_rate);
     // WP_Nav has set the vertical position control targets
